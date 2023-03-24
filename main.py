cafe3790e1426e9c7388a41a0d5a73f8cf90a94e
Soc-pr <audrius.priveda@gmail.com>
2023-03-24 13:03:39 +0200

    perdarius į set() atrodo viskas veikia. Liko padaryti: 1. Laimėjimą 2. Lygiąsias

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ lentele = [["-", "-", "-"],
            ["-", "-", "-"]]
 
 print(tabulate(lentele, tablefmt="fancy_grid"))
-judesiukai = []
+judesiukai = set()
 
 while True:
     x_move = int(input("Padekite X: "))
+    for sk_x in judesiukai:
+        if sk_x == x_move:
+            print("Reiksme yra, pasirenkam nauja")
+            x_move = int(input("vedam: "))
     if x_move == 7:
         for skaicius in lentele:
             del lentele[0][0]
@@ -53,62 +57,54 @@ while True:
         for skaicius in lentele:
             del lentele[2][2]
             lentele[2].insert(2, "X")
-
-
     print(tabulate(lentele, tablefmt="fancy_grid"))
 
+    judesiukai.add(x_move)
+
     _0_move = int(input("Padekite 0: "))
-    for langelis in judesiukai:
-        if _0_move == langelis:
-            _0_move = input("Reiksme yra, pasirenkam nauja: ")
+    for sk0 in judesiukai:
+        if sk0 == _0_move:
+            print("Reiksme yra, pasirenkam nauja")
+            _0_move = int(input("vedam: "))
+    if _0_move == 7:
+        for skaicius in lentele:
+            del lentele[0][0]
+            lentele[0].insert(0, "0")
+    elif _0_move == 8:
+        for skaicius in lentele:
+            del lentele[0][1]
+            lentele[0].insert(1, "0")
+    elif _0_move == 9:
+        for skaicius in lentele:
+            del lentele[0][2]
+            lentele[0].insert(2, "0")
+    elif _0_move == 4:
+        for skaicius in lentele:
+            del lentele[1][0]
+            lentele[1].insert(0, "0")
+    elif _0_move == 5:
+        for skaicius in lentele:
+            del lentele[1][1]
+            lentele[1].insert(1, "0")
+    elif _0_move == 6:
+        for skaicius in lentele:
+            del lentele[1][2]
+            lentele[1].insert(2, "0")
+    elif _0_move == 1:
+        for skaicius in lentele:
+            del lentele[2][0]
+            lentele[2].insert(0, "0")
+    elif _0_move == 2:
+        for skaicius in lentele:
+            del lentele[2][1]
+            lentele[2].insert(1, "0")
+    elif _0_move == 3:
+        for skaicius in lentele:
+            del lentele[2][2]
+            lentele[2].insert(2, "0")
+    judesiukai.add(_0_move)
 
-            if _0_move == 7:
-                for skaicius in lentele:
-                    del lentele[0][0]
-                    lentele[0].insert(0, "0")
 
-            elif _0_move == 8:
-                for skaicius in lentele:
-                    del lentele[0][1]
-                    lentele[0].insert(1, "0")
-
-            elif _0_move == 9:
-                for skaicius in lentele:
-                    del lentele[0][2]
-                    lentele[0].insert(2, "0")
-
-            elif _0_move == 4:
-                for skaicius in lentele:
-                    del lentele[1][0]
-                    lentele[1].insert(0, "0")
-
-            elif _0_move == 5:
-                for skaicius in lentele:
-                    del lentele[1][1]
-                    lentele[1].insert(1, "0")
-
-            elif _0_move == 6:
-                for skaicius in lentele:
-                    del lentele[1][2]
-                    lentele[1].insert(2, "0")
-
-            elif _0_move == 1:
-                for skaicius in lentele:
-                    del lentele[2][0]
-                    lentele[2].insert(0, "0")
-
-            elif _0_move == 2:
-                for skaicius in lentele:
-                    del lentele[2][1]
-                    lentele[2].insert(1, "0")
-
-            elif _0_move == 3:
-                for skaicius in lentele:
-                    del lentele[2][2]
-                    lentele[2].insert(2, "0")
-
-    judesiukai.append(x_move)
-    judesiukai.append(_0_move)
 
     print(judesiukai)
     print(tabulate(lentele, tablefmt="fancy_grid"))
